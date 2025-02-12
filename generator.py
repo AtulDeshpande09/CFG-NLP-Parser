67838f5a3a4d3f20d42b712e32bbe187ea293f83
Atul Deshpande <159980686+AtulDeshpande09@users.noreply.github.com>
2025-02-12 16:45:30 +0530

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,34 @@
+import random
+
+pcfg = {
+    "S": [("NP VP", 0.85) , ("S conj S" , 0.15) ], 
+    "NP": [("Det N", 0.3), ("Name", 0.3) , ("Det JJ N",0.4) ],
+    "VP": [("V NP", 0.95), ("V", 0.05) ],  
+    "Det": [("the", 0.3), ("a", 0.7)],
+    "N": [("cat", 0.3), ("dog", 0.3), ("mouse", 0.4)],
+    "V": [("sees", 0.5), ("chases", 0.5)],
+    "Name": [("Alice", 0.5), ("Bob", 0.5)],
+    "conj" : [("and", 0.5), ("but", 0.5)],
+    "JJ" : [("big",0.25),("little",0.25) , ("white",0.25) ,("black",0.25) ]
+}
+
+def generate(symbol:str):
+    
+    if symbol not in pcfg:
+        return symbol
+
+    #Here we expand the production rule
+    #IT chooses based on probability
+    rule_expanded = random.choices(
+            [sym[0] for sym in pcfg[symbol]],
+            weights = [sym[1] for sym in pcfg[symbol]]
+            )[0]
+
+    return " ".join(generate(sym) for sym in rule_expanded.split())
+
+
+# testing
+# Lets generate 10 sentences to check the result
+for _ in range(10):
+    print(generate("S"))
+    print("-----+----- ")

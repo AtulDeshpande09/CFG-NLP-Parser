c983ecaaf96959b7df38679c4bbc21f56cb7ee54
Atul Deshpande <159980686+AtulDeshpande09@users.noreply.github.com>
2025-02-13 18:08:46 +0530

    Update generator.py - Grammar Expanded

--- generator.py
+++ generator.py
@@ -1,13 +1,30 @@
 import random
 
+
 pcfg = {
-    "S": [("NP VP", 0.85) , ("S conj S" , 0.15) ], 
+    "S": [("NP VP", 0.85) , ("S conj S" , 0.15) ],
     "NP": [("Det N", 0.3), ("Name", 0.3) , ("Det JJ N",0.4) ],
-    "VP": [("V NP", 0.95), ("V", 0.05) ],  
+   
+    "VP": [("V NP", 0.15), ("V", 0.01) ,
+           ("VBD NP",0.13), ("VBZ VBG NP",0.13),("PP VBN NP",0.13),
+           ("MD RT NP", 0.16), ("MD be VBG NP" , 0.15),
+           ("PP been VBG NP" , 0.07) , ("MD have been VBG NP",0.07)],
+   
     "Det": [("the", 0.3), ("a", 0.7)],
     "N": [("cat", 0.3), ("dog", 0.3), ("mouse", 0.4)],
+   
     "V": [("sees", 0.5), ("chases", 0.5)],
+    "VBD": [("saw", 0.5), ("chased", 0.5)],
+    "VBZ": [("was", 0.5), ("is", 0.5)],
+    "VBG": [("seeing", 0.5), ("chasing", 0.5)],
     "Name": [("Alice", 0.5), ("Bob", 0.5)],
+   
+    "VBN": [("seen", 0.5), ("chased", 0.5)],
+    "PP": [("has", 0.5), ("had", 0.5)],
+   
+    "MD": [("will", 0.95), ("shall", 0.05)],
+    "RT": [("see", 0.5), ("chase", 0.5)],
+   
     "conj" : [("and", 0.5), ("but", 0.5)],
     "JJ" : [("big",0.25),("little",0.25) , ("white",0.25) ,("black",0.25) ]
 }
